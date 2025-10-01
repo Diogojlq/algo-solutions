bca43bc26c6379c1f9837073824d6d8e1b05afd0
Diogo <fmlsp2015@gmail.com>
2025-10-01 11:03:36 -0300

    easy: Two sum (correct)

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -1,6 +1,8 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
-    }
-};
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}  # valor -> índice
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
